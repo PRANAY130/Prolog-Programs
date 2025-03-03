9e8353f1bf98bdc87159bdb51a9e8e7b83e74612
Alhaitham <pranayde201@gmail.com>
2025-03-03 21:55:42 +0530

    modulo

--- operators.pl
+++ operators.pl
@@ -2,3 +2,4 @@ sum_numbers(A,B,Sum):-Sum is A+B.
 difference(A,B,Diff):-Diff is A-B.
 multiplication(A,B,Product):-Product is A*B.
 division(A,B,Quotient):-Quotient is A / B.
+modulo(A,B,Mod):-Mod is A mod B.
